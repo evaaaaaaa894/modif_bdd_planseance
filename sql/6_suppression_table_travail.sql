e5df3ffd508fd6f469e090a4ed5afd637860ad44
eva <eva24riviere@icloud.com>
2026-02-16 14:14:41 +0100

    Add files via upload

---
+++ sql/6_suppression_table_travail.sql
@@ -0,0 +1,27 @@
+BEGIN;
+
+-- nom de schéma définitif
+SET search_path TO psch;
+
+
+
+--tables à supp : temporaire
+
+DROP table tmp_cnc ;
+
+DROP table tmp_etab_cine ;
+
+DROP table tmp_programmation ;
+
+DROP table tmp_rsa ;
+
+DROP table tmp_rsa_cnc ;
+
+DROP table tmp_titre ;
+
+DROP table tmp_wiki1 ;
+
+DROP table tmp_wiki2 ;
+
+
+commit ;
